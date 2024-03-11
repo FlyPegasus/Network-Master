7325a0edaff203992862f13b457907aeba6bd9e0
Anshu Das <107601282+FlyPegasus@users.noreply.github.com>
2024-03-11 16:38:24 +0530

    Add files via upload

---
+++ client.c
@@ -0,0 +1,51 @@
+// normal client program
+#include<stdio.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+
+#define PORT 9008
+int main(void){
+    struct sockaddr_in server;
+    char msg[50];
+    // Initializing Socket
+    /*
+        AF_INET[domain]: address family(IPv4 / IPv6)
+        SOCK_STREAM[communication type]: connection oriented socket (TCP)
+        SOCK_DGRAM[communication type]: UDP
+        0[protocol]: default protocol of system
+        IPPROTO_TCP[protocol]: using the TCP protocol
+    */
+    int newSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (newSocket < 0)
+    {
+        printf("Failed\n");
+    }
+    else{
+        printf("Success\n");
+    }
+
+    // Initialize environment for sockaddr structure
+    /*
+        inet_addr("ip") can be replaced by Macro INADDR_ANY
+    */
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Connecting to server
+    int newConnection = connect(newSocket, (struct sockaddr*) &server, sizeof(server));
+    if (newConnection < 0)
+    {
+        printf("Connection Failed\n");
+    }
+    else{
+        printf("Connected\n");
+        printf("Enter your message: ");
+        scanf("%[^\n]s", msg);
+        send(newSocket, msg, sizeof(msg), 0);
+    }
+    close(newSocket);
+
+     return 0;
+}
---
+++ server.c
@@ -0,0 +1,82 @@
+// normal server program
+#include<stdio.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+
+#define PORT 9008
+int main(void){
+    char buff[50];
+    // Initializing Socket
+    /*
+        AF_INET[domain]: address family(IPv4 / IPv6)
+        SOCK_STREAM[communication type]: connection oriented socket (TCP)
+        SOCK_DGRAM[communication type]: UDP
+        0[protocol]: default protocol of system
+        IPPROTO_TCP[protocol]: using the TCP protocol
+    */
+    int newSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (newSocket < 0)
+    {
+        printf("Failed\n");
+    }
+    else{
+        printf("Success\n");
+    }
+
+    // Initialize environment for sockaddr structure
+    /*
+        inet_addr("ip") can be replaced by Macro INADDR_ANY
+    */
+    struct sockaddr_in server, new_conn;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Binding the socket to localhost
+    int newBind = bind(newSocket, (struct sockaddr*) &server, sizeof(server));
+    if (newBind < 0)
+    {
+        printf("Bind unsuccessful\n");
+    }
+    else{
+        printf("Bind success\n");
+    }
+
+    int newListen = listen(newSocket, 1); // Second argument: the number of client requests to be handled
+    if (newListen < 0)
+    {
+        printf("Port not listening\n");
+    }
+    else{
+        printf("Port listening\n");
+    }
+
+
+    // Accepting the incoming connections
+    int new_connLen = sizeof(new_conn);
+    int newConnection = accept(newSocket, (struct sockaddr*) &new_conn, &new_connLen);
+    if (newConnection < 0)
+    {
+        printf("Connection not accepted\n");
+    }
+    else{
+        printf("Connection accepted\n");
+        printf("Connection descriptor: %d\n", newConnection);
+    }
+    // recieving data and storing into buffer
+    int msgLen = recv(newConnection, buff, sizeof(buff), 0); 
+    if (msgLen < 0)
+    {
+        printf("Message Transfer failed\n");
+    }
+    else{
+        printf("Msg recieved of length: %d\n", msgLen);
+        printf("Client Says: %s\n", buff);
+    }
+    close(newConnection);
+    close(newBind);
+     return 0;
+    
+    
+}
